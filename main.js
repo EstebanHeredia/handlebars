3315704ad4c7d6aaae22e7fb5290404bb7146c7b
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2022-05-19 00:26:43 -0500

    Corregir hasta el momento
    Comento las líneas que sobran hasta el momento y agrego lo que está faltando para que vaya.

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const productos = [];
-const app = require("app");
+// const app = require("app");
+const app = express()
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 // app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use;
+// app.set("views", __dirname + "/views");
+// app.use;
 app.get("/", (req, res) => {
-  res.render("../views/index"), { productos };
+  // res.render("../views/index"), { productos };
+  res.render("index");
+});
+
+app.get("/productos", (req, res) => {
+  res.render("resumen", { productos });
 });
 
 app.post("/productos", (req, res) => {
@@ -20,7 +26,8 @@ app.post("/productos", (req, res) => {
 
 const PORT = 8080;
 const server = app.listen(PORT, () => {
-  console.log("Servidor en escucha en el puerto: "`${server.address().port}`);
+  console.log(`Servidor en escucha en el puerto: ${server.address().port}`);
+  // console.log("Servidor en escucha en el puerto: "`${server.address().port}`);
 });
 
 server.on("error", (error) => console.log(`Error en el servidor ${error}`));
